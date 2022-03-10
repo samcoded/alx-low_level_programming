cde2f5699ebebd204673a5912f0bbe4ee88e9c73
Samuel Anozie <samuelanozie10@yahoo.com>
2022-03-10 13:46:51 +0100

    fix 101 quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, q, sizeof(q));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
