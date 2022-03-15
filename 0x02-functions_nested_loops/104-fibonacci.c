c00ed2f79a15011f65005d4068c26b8e241136ce
Samuel Anozie <samuelanozie10@yahoo.com>
2022-03-15 17:47:18 +0100

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 98)
+	while (i < 100)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 97)
+		if (i < 99)
 			printf(", ");
 		i++;
 	}
