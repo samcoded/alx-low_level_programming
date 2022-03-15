c9ca89c8e8e40cad1bac25055a07f12e28725ad4
Samuel Anozie <samuelanozie10@yahoo.com>
2022-03-15 16:37:14 +0100

    fix task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,42 @@
-#include "main.h"
-
+// #include "main.h"
+#include "_putchar.c"
+#include <stdio.h>
 /**
  * print_to_98 - prints from a given number up to 98
  * @n: the number to start from
+
  * Return: Nothing, Void
  */
 void print_to_98(int n)
 {
+	int t, d;
+
 	if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
 			if (n != 98)
 			{
-				printf("%d, ", n);
+				if (n > 99)
+				{
+					d = n / 100;
+					_putchar(d % 10 + '0');
+				}
+
+				if (n > 9)
+				{
+					t = n / 10;
+					_putchar(t % 10 + '0');
+				}
+				_putchar(n % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
 			else
 			{
-				printf("%d", n);
+				_putchar('9');
+				_putchar('8');
+				_putchar('\n');
 			}
 		}
 	}
@@ -25,15 +44,51 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			if (n != 98)
+			if (n < 0)
 			{
-				printf("%d, ", n);
+				_putchar('-');
+				if (n < -9)
+				{
+					t = n / 10;
+					_putchar((t * -1) % 10 + '0');
+				}
+				_putchar((n * -1) % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (n != 98)
+			{
+				if (n > 99)
+				{
+					d = n / 100;
+					_putchar(d % 10 + '0');
+				}
+
+				if (n > 9)
+				{
+					t = n / 10;
+					_putchar(t % 10 + '0');
+				}
+				_putchar(n % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
 			else
 			{
-				printf("%d", n);
+				_putchar('9');
+				_putchar('8');
+				_putchar('\n');
 			}
 		}
 	}
-	printf("\n");
+}
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
 }
