3499c89ded17ee192e125971fe2c4d18571a01e8
Samuel Anozie <samuelanozie10@yahoo.com>
2022-03-21 19:09:04 +0100

    fix atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _atoi - xonvert to integer
+ * @s: poiter to string
+ * Return: integer
+ */
 int _atoi(char *s)
 {
 	unsigned int i = 0, z = 0, p = 0;
@@ -36,27 +41,4 @@ int _atoi(char *s)
 	}
 
 	return (p * h);
-}
-
-int main(void)
-{
-	int nb;
-
-	nb = _atoi("98");
-	printf("%d\n", nb);
-	nb = _atoi("-402");
-	printf("%d\n", nb);
-	nb = _atoi("          ------++++++-----+++++--98");
-	printf("%d\n", nb);
-	nb = _atoi("214748364");
-	printf("%d\n", nb);
-	nb = _atoi("0");
-	printf("%d\n", nb);
-	nb = _atoi("Suite 402");
-	printf("%d\n", nb);
-	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-	printf("%d\n", nb);
-	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", nb);
-	return (0);
 }
