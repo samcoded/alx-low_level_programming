531b3782d1d36b83d0563d14bab3f9dd4191873a
Samuel Anozie <samuelanozie10@yahoo.com>
2022-03-21 17:56:03 +0100

    fix puts 1

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts - ouput
  * @str: string
@@ -6,11 +7,9 @@
  */
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
