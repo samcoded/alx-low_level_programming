2e8da54d7d17f29926cbd0e0e5c8403666dc6c4f
Samuel Anozie <samuelanozie10@yahoo.com>
2022-03-21 18:16:59 +0100

    puts2 fix 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,13 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
-	{
+	int i;
 
-		if (*str % 2 == 0)
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
 		{
-			_putchar(*str++);
+			_putchar(str[i]);
 		}
 	}
 	_putchar('\n');
