7029a64f5be2eb6d53dee2b75b2e6aab3a531569
Samuel Anozie <samuelanozie10@yahoo.com>
2022-04-08 00:40:02 +0100

    101 mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -29,4 +29,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+void _is_zero(char *argv[]);
+char *_initialize_array(char *ar, int lar);
+int _checknum(char *argv[], int n);
+
 #endif
